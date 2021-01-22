7504c60147a66377840bc805e51f134abdff0f2a
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-21 22:09:23 -0300

    Commit inicial

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataDrivenAPI.Data;
+using DataDrivenAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("categories")]
+public class CategoryController : ControllerBase
+{
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<List<Category>>> Get()
+    {
+        return new List<Category>();
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> GetById(int id)
+    {
+        return new Category();
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<Category>> Post([FromBody]Category model, [FromServices] DataContext context)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        try
+        {
+            context.Categories.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch
+        {
+            return BadRequest(new { message = "Não foi possível criar a categoria."});
+        }
+        
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model, [FromServices] DataContext context)
+    {
+        if(id != model.Id)
+            return NotFound(new { message = "Categoria não encontrada"});
+
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        try
+        {
+            context.Entry<Category>(model).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            return BadRequest(new { message = "Este registro já foi atualizado."});
+        }
+        catch
+        {
+            return BadRequest(new { message = "Não foi possível atualizar a categoria."});
+        }
+    }
+
+    [HttpDelete]
+    [Route("")]
+    public async Task<ActionResult<Category>> Delete()
+    {
+        return Ok();
+    }
+}
