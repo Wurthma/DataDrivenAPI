7d373434697976f7eefd6a1dfcde90cfd3c05d92
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-26 22:11:24 -0300

    teste...

--- Startup.cs
+++ Startup.cs
@@ -64,12 +64,12 @@ namespace DataDrivenAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            // if (env.IsDevelopment())
+            // {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DataDrivenAPI v1"));
-            }
+            // }
 
             app.UseHttpsRedirection();
 
