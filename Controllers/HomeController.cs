10bd6481251fa0fd7babfb988f7f70a3c547f08c
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-26 22:20:12 -0300

    Remove valor explicito dos IDs a serem adicionados (EF)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,10 +12,10 @@ namespace DataDrivenAPI.Controllers
         [Route("")]
         public async Task<ActionResult<dynamic>> Get([FromServices] DataContext context)
         {
-            var employee = new User { Id = 1, Username = "robin", Password = "robin", Role = "employee" };
-            var manager = new User { Id = 2, Username = "batman", Password = "batman", Role = "manager" };
-            var category = new Category { Id = 1, Title = "Informática" };
-            var product = new Product { Id = 1, Category = category, Title = "Mouse", Price = 299, Description = "Mouse Gamer" };
+            var employee = new User { Username = "robin", Password = "robin", Role = "employee" };
+            var manager = new User { Username = "batman", Password = "batman", Role = "manager" };
+            var category = new Category { Title = "Informática" };
+            var product = new Product { Category = category, Title = "Mouse", Price = 299, Description = "Mouse Gamer" };
             context.Users.Add(employee);
             context.Users.Add(manager);
             context.Categories.Add(category);
